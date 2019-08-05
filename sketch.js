f593a1c5cc2d90f20d521fe235ef280b23e92fab
Jim <jim@advancebass.com>
2019-08-05 16:38:41 -0700

    add random math lib and create 1st generative art

--- sketch.js
+++ sketch.js
@@ -1,25 +1,43 @@
 const canvasSketch = require("canvas-sketch");
+const { lerp } = require("canvas-sketch-util/math");
+const random = require("canvas-sketch-util/random");
 
 const settings = {
-  dimensions: [2048, 2048],
-  // units: 'in',
-  // dimensions: "A4",
-  // orientation: 'portrait',
-  // pixelsPerInch: 300
+  dimensions: [2048, 2048]
 };
 
 const sketch = () => {
+  const createGrid = () => {
+    const points = [];
+    const count = 20;
+    for (let x = 0; x < count; x++) {
+      for (let y = 0; y < count; y++) {
+        const u = count <= 1 ? 0.5 : x / (count - 1);
+        const v = count <= 1 ? 0.5 : y / (count - 1);
+        points.push([u, v]);
+      }
+    }
+    return points;
+  };
+
+  random.setSeed(2006);
+  const points = createGrid().filter(() => random.value() > 0.5);
+  const margin = 256;
+
   return ({ context, width, height }) => {
-    context.fillStyle = "skyblue";
+    context.fillStyle = "white";
     context.fillRect(0, 0, width, height);
 
-    context.beginPath();
-    context.arc(width / 2, height / 2, width * 0.2, 0, Math.PI * 2, false);
-    context.fillStyle = "coral";
-    context.fill();
-    context.lineWidth = width * 0.03;
-    context.strokeStyle = "slategrey";
-    context.stroke();
+    points.forEach(([u, v]) => {
+      const x = lerp(margin, width - margin, u);
+      const y = lerp(margin, height - margin, v);
+
+      context.beginPath();
+      context.arc(x, y, 16, 0, Math.PI * 2, false);
+      context.strokeStyle = "darkgray";
+      context.lineWidth = 4;
+      context.stroke();
+    });
   };
 };
 
