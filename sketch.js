b7498da13fa532deeb604af5dcc9d9805b5296f0
Jim <jim@advancebass.com>
2019-08-11 16:43:09 -0700

    add noise2D randomness

--- sketch.js
+++ sketch.js
@@ -18,9 +18,10 @@ const sketch = () => {
       for (let y = 0; y < count; y++) {
         const u = count <= 1 ? 0.5 : x / (count - 1);
         const v = count <= 1 ? 0.5 : y / (count - 1);
+        const radius = Math.abs(random.noise2D(u, v) * 0.05);
         points.push({
           color: random.pick(palette),
-          radius: Math.abs(0.01 + random.gaussian() * 0.01),
+          radius,
           position: [u, v]
         });
       }
