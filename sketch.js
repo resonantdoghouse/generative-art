a4ab0b621a83e4f8e54fe3543387006ad8b35744
Jim <jim@advancebass.com>
2019-08-11 17:00:18 -0700

    add random seed option and export image suffix with seed number

--- sketch.js
+++ sketch.js
@@ -3,7 +3,10 @@ const { lerp } = require("canvas-sketch-util/math");
 const random = require("canvas-sketch-util/random");
 const palettes = require("nice-color-palettes");
 
+random.setSeed(random.getRandomSeed());
+
 const settings = {
+  suffix: random.getSeed(),
   dimensions: [2048, 2048]
 };
 
